86fc2d0aa5a53d68b3e0543d6b07829536d8b97e
Pat O'Callaghan <patocallaghan@gmail.com>
2018-12-03 10:38:44 +0000

    formatting

--- 2018/day1/elixir/part1.exs
+++ 2018/day1/elixir/part1.exs
@@ -1,23 +1,20 @@
 # https://adventofcode.com/2018/day/1
 
-
 defmodule FrequencySummer do
-
   def sum_freqs(input) do
     Enum.sum(input)
   end
-
 end
 
 case System.argv() do
- ["--test"] ->
-    ExUnit.start();
+  ["--test"] ->
+    ExUnit.start()
 
     defmodule FrequencySummerTest do
       use ExUnit.Case
 
-      import FrequencySummer;
-      
+      import FrequencySummer
+
       test "range" do
         assert sum_freqs([+1, +1, +1]) == 3
         assert sum_freqs([+1, +1, -2]) == 0
@@ -27,12 +24,12 @@ case System.argv() do
 
   [input_file] ->
     File.stream!(input_file)
-    |> Stream.map( &(String.replace(&1, "\n", "")) )
-    |> Enum.map( &(String.to_integer(&1)) )
-    |> FrequencySummer.sum_freqs
-    |> IO.puts
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.map(&String.to_integer(&1))
+    |> FrequencySummer.sum_freqs()
+    |> IO.puts()
 
   _ ->
-    IO.puts :stderr, "We expected --test or an input file"
+    IO.puts(:stderr, "We expected --test or an input file")
     System.halt(1)
 end
--- 2018/day1/elixir/part2.exs
+++ 2018/day1/elixir/part2.exs
@@ -1,22 +1,24 @@
 # https://adventofcode.com/2018/day/1
 
 defmodule FrequencyFinder do
-
   def get_dupe_freq(numbers) do
     frequencies = MapSet.new()
-    calculate_frequencies(numbers, frequencies, 0);
+    calculate_frequencies(numbers, frequencies, 0)
   end
 
   defp calculate_frequencies(numbers, frequencies, count) do
-    { is_found, frequency, frequencies } = Enum.reduce_while(numbers, {:not_found, count, frequencies }, fn (n, { _, accum, frequencies }) ->
-      accum = accum + n
-      if MapSet.member?(frequencies, accum) do
-        { :halt, { :found, accum, frequencies } }
-      else
-        frequencies = MapSet.put(frequencies, accum)
-        { :cont, { :not_found, accum, frequencies } }
-      end
-    end)
+    {is_found, frequency, frequencies} =
+      Enum.reduce_while(numbers, {:not_found, count, frequencies}, fn n,
+                                                                      {_, accum, frequencies} ->
+        accum = accum + n
+
+        if MapSet.member?(frequencies, accum) do
+          {:halt, {:found, accum, frequencies}}
+        else
+          frequencies = MapSet.put(frequencies, accum)
+          {:cont, {:not_found, accum, frequencies}}
+        end
+      end)
 
     if is_found == :found do
       frequency
@@ -27,14 +29,14 @@ defmodule FrequencyFinder do
 end
 
 case System.argv() do
- ["--test"] ->
-    ExUnit.start();
+  ["--test"] ->
+    ExUnit.start()
 
     defmodule FrequencyFinderTest do
       use ExUnit.Case
 
-      import FrequencyFinder;
-      
+      import FrequencyFinder
+
       test "range" do
         assert get_dupe_freq([+3, +3, +4, -2, -4]) == 10
         assert get_dupe_freq([-6, +3, +8, +5, -6]) == 5
@@ -43,12 +45,14 @@ case System.argv() do
     end
 
   [input_file] ->
-    input = File.stream!(input_file)
-    |> Stream.map( &(String.replace(&1, "\n", "")) )
-    |> Enum.map(fn(line) -> String.to_integer(line) end)
-    IO.puts FrequencyFinder.get_dupe_freq(input)
+    input =
+      File.stream!(input_file)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+      |> Enum.map(fn line -> String.to_integer(line) end)
+
+    IO.puts(FrequencyFinder.get_dupe_freq(input))
 
   _ ->
-    IO.puts :stderr, "We expected --test or an input file"
+    IO.puts(:stderr, "We expected --test or an input file")
     System.halt(1)
 end
